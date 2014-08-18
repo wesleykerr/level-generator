3f71913ddd20e6d4cff554c6a1fdf469b8ffca09
Wesley Kerr <wesley.n.kerr@gmail.com>
2014-08-18 07:41:32 -0700

    Initial commit -- bare bones project.
    We will be building on top of this project and I want to ensure that
    we keep track of the changes.

---
+++ desktop/src/com/seekerr/games/generator/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.seekerr.games.generator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.seekerr.games.generator.ProceduralLevelGenerator;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProceduralLevelGenerator(), config);
+	}
+}
