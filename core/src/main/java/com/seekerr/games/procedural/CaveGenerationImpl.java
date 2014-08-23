ebda4b4f2ad89c6a4705640b3b51aa5cab505439
Wesley Kerr <wesley.n.kerr@gmail.com>
2014-08-23 11:11:20 -0700

    Updated the Cave generation algorithm for better output
    Added in controls to Screen for reinitializing the world and stepping
    through cave generation.

--- core/src/main/java/com/seekerr/games/procedural/CaveGenerationImpl.java
+++ core/src/main/java/com/seekerr/games/procedural/CaveGenerationImpl.java
@@ -14,13 +14,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
 /**
  * This is the implementation of a cave generation algorithm detailed:
- *     http://www.roguebasin.com/index.php?title=Cellular_Automata_Method_for_Generating_Random_Cave-Like_Levels
- * Even though this was originally designed for generating caves, it 
- * presents natural looking structures that from the top down could be 
- * considered internals of forests or an area where people can walk around.
+ * http://www.roguebasin.com/index.php?title=
+ * Cellular_Automata_Method_for_Generating_Random_Cave-Like_Levels Even though
+ * this was originally designed for generating caves, it presents natural
+ * looking structures that from the top down could be considered internals of
+ * forests or an area where people can walk around.
  * 
  * @author wkerr
  *
@@ -33,35 +35,55 @@ public class CaveGenerationImpl {
     // we can do a simple count to find the number of walls surrounding us.
     public static final boolean EMPTY = false;
     public static final boolean WALL = true;
-    
+
     private long seed;
     private Random random;
 
     private boolean[][] map;
-    
+    private boolean[][] bufferMap;
+
     private int width;
     private int height;
 
+    private List<Phase> phases;
+
     private CaveGenerationImpl() {
         this.seed = 7;
+        this.phases = Lists.newArrayList();
     }
-    
+
+    /**
+     * Return the map for rendering.
+     * 
+     * @return
+     */
     public boolean[][] getMap() {
         return map;
     }
-    
+
+    /**
+     * Return the parameters for the phase.
+     * 
+     * @param index
+     * @return
+     */
+    public Phase getPhase(int index) {
+        return phases.get(index);
+    }
+
     /**
      * We count the cell that is requested as well.
+     * 
      * @param map
      * @param row
      * @param col
      * @return
      */
-    @VisibleForTesting 
+    @VisibleForTesting
     int getNeighborCount(boolean[][] map, int row, int col) {
         int count = 0;
-        for (int i = row-1; i <= row+1; ++i) {
-            for (int j = col-1; j <= col+1; ++j) {
+        for (int i = row - 1; i <= row + 1; ++i) {
+            for (int j = col - 1; j <= col + 1; ++j) {
                 if (i < 0 || j < 0 || i >= map.length || j > map[i].length) {
                     continue;
                 }
@@ -70,9 +92,10 @@ public class CaveGenerationImpl {
         }
         return count;
     }
-    
+
     /**
      * We count the cell that is requested as well.
+     * 
      * @param map
      * @param row
      * @param col
@@ -81,9 +104,9 @@ public class CaveGenerationImpl {
     @VisibleForTesting
     int getTwoStepNeighborCount(boolean[][] map, int row, int col) {
         int count = 0;
-        for (int i = row-2; i <= row+2; ++i) {
-            for (int j = col-2; j <= col+2; ++j) {
-                if (Math.abs(i-row) == 2 && Math.abs(j-col) == 2) {
+        for (int i = row - 2; i <= row + 2; ++i) {
+            for (int j = col - 2; j <= col + 2; ++j) {
+                if (Math.abs(i - row) == 2 && Math.abs(j - col) == 2) {
                     continue;
                 }
                 if (i < 0 || j < 0 || i >= map.length || j >= map[i].length) {
@@ -94,18 +117,18 @@ public class CaveGenerationImpl {
         }
         return count;
     }
-    
-    public void generate() {
+
+    public void initialize() {
         random = new Random(seed);
+
         map = new boolean[height][width];
-        boolean[][] bufferMap = new boolean[height][width];
-        // We go from 1 to 98 since the outer will always be walls.
-        for (int i = 0; i < height ; ++i) {
-            for (int j = 0; j < width ; ++j) {
+        bufferMap = new boolean[height][width];
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
                 bufferMap[i][j] = WALL;
 
-                if (i == 0 || j == 0 || i == map.length - 1 || 
-                        j == map[i].length - 1) {
+                if (i == 0 || j == 0 || i == map.length - 1
+                        || j == map[i].length - 1) {
                     map[i][j] = WALL;
                     continue;
                 }
@@ -115,47 +138,41 @@ public class CaveGenerationImpl {
                 }
             }
         }
-        
-        System.out.println("Initial:");
-        System.out.println(toString(map));
-        boolean[][] tmpMap;
-        for (int k = 0; k < 4; ++k) {
-            for (int i = 1; i < height - 1; ++i) {
-                for (int j = 1; j < width - 1; ++j) {
-                    int count1 = getNeighborCount(map, i, j);
-                    int count2 = getTwoStepNeighborCount(map, i, j);
-                    if (count1 >= 5 || count2 <= 2) {
-                        bufferMap[i][j] = WALL;
-                    } else {
-                        bufferMap[i][j] = EMPTY;
-                    }
-                }
-            }
-            tmpMap = map;
-            map = bufferMap;
-            bufferMap = tmpMap;
+        Gdx.app.log(TAG, "Initial");
+        Gdx.app.log(TAG, toString(map));
+    }
 
-            System.out.println("Round: " + k);
-            System.out.println(toString(map));
+    public void step(int minCount, int maxCount) {
+        // if we haven't called initialize yet
+        // go ahead and do it it ourselves.
+        if (bufferMap == null) {
+            initialize();
         }
-        
-        for (int k = 0; k < 3; ++k) { 
-            for (int i = 1; i < height - 1; ++i) {
-                for (int j = 1; j < width - 1; ++j) {
-                    int count1 = getNeighborCount(map, i, j);
-                    if (count1 >= 5) {
-                        bufferMap[i][j] = WALL;
-                    } else {
-                        bufferMap[i][j] = EMPTY;
-                    }
+        boolean[][] tmpMap;
+        for (int i = 1; i < height - 1; ++i) {
+            for (int j = 1; j < width - 1; ++j) {
+                int count1 = getNeighborCount(map, i, j);
+                int count2 = getTwoStepNeighborCount(map, i, j);
+                if (count1 >= minCount || count2 <= maxCount) {
+                    bufferMap[i][j] = WALL;
+                } else {
+                    bufferMap[i][j] = EMPTY;
                 }
             }
-            tmpMap = map;
-            map = bufferMap;
-            bufferMap = tmpMap;
+        }
+        tmpMap = map;
+        map = bufferMap;
+        bufferMap = tmpMap;
+    }
 
-            System.out.println("Cleanup: " + k);
-            System.out.println(toString(map));
+    public void generate() {
+        initialize();
+        for (Phase p : phases) {
+            for (int i = 0; i < p.rounds; ++i) {
+                step(p.min, p.max);
+                Gdx.app.log(TAG, "Round: " + i);
+                Gdx.app.log(TAG, toString(map));
+            }
         }
 
         System.out.println(toString(map));
@@ -167,28 +184,28 @@ public class CaveGenerationImpl {
             fixRoom(rooms.get(i));
         }
     }
-    
-    public void addNeighbor(Point current, int dx, int dy, 
+
+    public void addNeighbor(Point current, int dx, int dy,
             LinkedHashSet<Point> notVisited, LinkedList<Point> frontier) {
-        Point neighbor = new Point(current.x+dx, current.y+dy);
-        if (neighbor.x < 0 || neighbor.y < 0 || 
-                neighbor.x >= width || neighbor.y >= height) 
+        Point neighbor = new Point(current.x + dx, current.y + dy);
+        if (neighbor.x < 0 || neighbor.y < 0 || neighbor.x >= width
+                || neighbor.y >= height)
             return;
-        
-        if (map[neighbor.y][neighbor.x] == EMPTY && 
-                notVisited.contains(neighbor)) {
+
+        if (map[neighbor.y][neighbor.x] == EMPTY
+                && notVisited.contains(neighbor)) {
             notVisited.remove(neighbor);
             frontier.addLast(neighbor);
         }
     }
-    
+
     public List<Set<Point>> getRooms() {
         Point start = null;
         LinkedHashSet<Point> notVisited = new LinkedHashSet<Point>();
-        for (int i = 0; i < height ; ++i) {
-            for (int j = 0; j < width ; ++j) {
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
                 if (map[i][j] == EMPTY) {
-                    if (start == null) 
+                    if (start == null)
                         start = new Point(j, i);
                     else
                         notVisited.add(new Point(j, i));
@@ -201,18 +218,18 @@ public class CaveGenerationImpl {
         frontier.add(start);
         while (!notVisited.isEmpty()) {
             Set<Point> room = new HashSet<Point>();
-            while (!frontier.isEmpty()) { 
+            while (!frontier.isEmpty()) {
                 Point current = frontier.removeFirst();
                 room.add(current);
-                
-                // getNeighbors() -- only allow moving up or down 
+
+                // getNeighbors() -- only allow moving up or down
                 addNeighbor(current, 1, 0, notVisited, frontier);
                 addNeighbor(current, -1, 0, notVisited, frontier);
                 addNeighbor(current, 0, 1, notVisited, frontier);
                 addNeighbor(current, 0, -1, notVisited, frontier);
             }
             rooms.add(room);
-            
+
             if (notVisited.size() > 0) {
                 Iterator<Point> iterator = notVisited.iterator();
                 frontier.add(iterator.next());
@@ -221,13 +238,13 @@ public class CaveGenerationImpl {
         }
         return rooms;
     }
-    
-        
+
     /**
-     * Choose a random point from the room and walk towards the center 
-     * of the map until we encounter an empty cell that is not part of
-     * this room.
-     * @param room - all of the points in this room.
+     * Choose a random point from the room and walk towards the center of the
+     * map until we encounter an empty cell that is not part of this room.
+     * 
+     * @param room
+     *            - all of the points in this room.
      */
     public void fixRoom(Set<Point> room) {
         Point point = room.iterator().next();
@@ -239,11 +256,14 @@ public class CaveGenerationImpl {
         int x = point.x;
         int y = point.y;
         while (x > 0 && y > 0 && x < width && y < height) {
-            if (random.nextDouble() < 0.5) 
+            if (random.nextDouble() < 0.5)
                 x += dx;
             else
                 y += dy;
-            
+
+            if (x < 0 || y < 0 || x >= width || y >= width)
+                break;
+
             point.setLocation(x, y);
             if (map[y][x] == EMPTY && !room.contains(point)) {
                 return;
@@ -251,68 +271,97 @@ public class CaveGenerationImpl {
 
             if (map[y][x] == WALL) {
                 map[y][x] = EMPTY;
-            } 
+            }
         }
-        
+
         Gdx.app.log(TAG, "Encountered a boundary before finding an open space!");
         Gdx.app.log(TAG, ".. last location: " + x + ", " + y);
     }
-    
+
     @Override
     public String toString() {
         return toString(map);
     }
-    
+
     public static String toString(boolean[][] map) {
         StringBuilder buf = new StringBuilder();
         for (int i = 0; i < map.length; ++i) {
             for (int j = 0; j < map[i].length; ++j) {
                 if (map[i][j] == EMPTY)
                     buf.append(".");
-                else 
+                else
                     buf.append("#");
             }
             buf.append("\n");
         }
         return buf.toString();
     }
-    
+
+    public static class Phase {
+        int min;
+        int max;
+        int rounds;
+
+        public Phase(int min, int max, int rounds) {
+            this.min = min;
+            this.max = max;
+            this.rounds = rounds;
+        }
+
+        public int getMin() {
+            return min;
+        }
+
+        public int getMax() {
+            return max;
+        }
+
+        public int getRounds() {
+            return rounds;
+        }
+    }
+
     public static class Builder {
         CaveGenerationImpl cave;
-        
+
         private Builder() {
             cave = new CaveGenerationImpl();
         }
-        
+
         public Builder withSize(int width, int height) {
             cave.width = width;
             cave.height = height;
             return this;
         }
-        
-        public Builder withWidth(int width) { 
+
+        public Builder withWidth(int width) {
             cave.width = width;
             return this;
         }
-        
-        public Builder withHeight(int height) { 
+
+        public Builder withHeight(int height) {
             cave.height = height;
             return this;
         }
-        
+
         public Builder withRandomSeed(long seed) {
             cave.seed = seed;
             return this;
         }
-        
-        public CaveGenerationImpl build() { 
+
+        public Builder addPhase(int min, int max, int rounds) {
+            cave.phases.add(new Phase(min, max, rounds));
+            return this;
+        }
+
+        public CaveGenerationImpl build() {
             Preconditions.checkNotNull(cave);
             CaveGenerationImpl tmp = cave;
             cave = null;
             return tmp;
         }
-        
-        public static Builder create() { 
+
+        public static Builder create() {
             return new Builder();
         }
 
--- core/src/main/java/com/seekerr/games/procedural/ProceduralGenerationScreen.java
+++ core/src/main/java/com/seekerr/games/procedural/ProceduralGenerationScreen.java
@@ -1,10 +1,10 @@
 package com.seekerr.games.procedural;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,8 +15,8 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.seekerr.games.generator.Assets;
+import com.seekerr.games.procedural.CaveGenerationImpl.Phase;
 
 /**
  * This screen is for rendering different levels and allowing a graphical
@@ -25,7 +25,7 @@ import com.seekerr.games.generator.Assets;
  * @author wkerr
  *
  */
-//TODO(wkerr): Extend ScreenAdapter instead of implementing Screen
+// TODO(wkerr): Extend ScreenAdapter instead of implementing Screen
 public class ProceduralGenerationScreen implements Screen {
     /** Tag used for logging purposes. */
     private static final String TAG = "ProceduralGenerationScreen";
@@ -44,20 +44,19 @@ public class ProceduralGenerationScreen implements Screen {
 
     private ShapeRenderer shapeRenderer;
     private SpriteBatch spriteBatch;
-    
+
     private boolean shouldDrawShapes = false;
-    
+
     public ProceduralGenerationScreen() {
         width = Gdx.graphics.getWidth();
         height = Gdx.graphics.getHeight();
     }
-    
-    private void generateCave() { 
-        caveGenerator = CaveGenerationImpl.Builder.create()
-                .withSize(60, 40)
-                .withRandomSeed(System.currentTimeMillis())
-                .build();
-        caveGenerator.generate();        
+
+    private void generateCave() {
+        caveGenerator = CaveGenerationImpl.Builder.create().withSize(60, 40)
+                .withRandomSeed(System.currentTimeMillis()).addPhase(5, 2, 4)
+                .addPhase(5, -1, 5).build();
+        caveGenerator.generate();
     }
 
     /**
@@ -73,12 +72,12 @@ public class ProceduralGenerationScreen implements Screen {
 
         spriteBatch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-        
+
         Gdx.input.setInputProcessor(new GameplayInput());
 
         Assets.assetManager.load("level-generator.pack", TextureAtlas.class);
         Assets.assetManager.finishLoading();
-        
+
         generateCave();
     }
 
@@ -86,11 +85,13 @@ public class ProceduralGenerationScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0.0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
+
         camera.update();
-        
-        int gridSize = 15;
-        if (shouldDrawShapes) 
+
+        int gridx = width / caveGenerator.getMap()[0].length;
+        int gridy = height / caveGenerator.getMap().length;
+        int gridSize = Math.min(gridx, gridy);
+        if (shouldDrawShapes)
             renderShapes(camera, gridSize);
         else
             renderSprites(camera, gridSize);
@@ -99,38 +100,40 @@ public class ProceduralGenerationScreen implements Screen {
         font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 20, 20);
         batch.end();
     }
-    
+
     private void renderSprites(Camera camera, int gridSize) {
-        TextureAtlas atlas = Assets.assetManager.get("level-generator.pack", TextureAtlas.class);
+        TextureAtlas atlas = Assets.assetManager.get("level-generator.pack",
+                TextureAtlas.class);
         TextureRegion floor = atlas.findRegion("floor");
         TextureRegion wall = atlas.findRegion("wall");
-        
+
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
         boolean[][] map = caveGenerator.getMap();
-        for (int i = 0; i < map.length; ++i) { 
-            for (int j = 0; j < map[i].length; ++j) { 
-                float x = j*gridSize;
-                float y = i*gridSize;
-                
-                TextureRegion region = map[i][j] == CaveGenerationImpl.WALL ? wall : floor;
+        for (int i = 0; i < map.length; ++i) {
+            for (int j = 0; j < map[i].length; ++j) {
+                float x = j * gridSize;
+                float y = i * gridSize;
+
+                TextureRegion region = map[i][j] == CaveGenerationImpl.WALL ? wall
+                        : floor;
                 spriteBatch.draw(region, x, y, gridSize, gridSize);
             }
         }
         spriteBatch.end();
     }
-    
+
     private void renderShapes(Camera camera, int gridSize) {
         shapeRenderer.setProjectionMatrix(camera.combined);
         shapeRenderer.begin(ShapeType.Filled);
-        
+
         boolean[][] map = caveGenerator.getMap();
-        for (int i = 0; i < map.length; ++i) { 
-            for (int j = 0; j < map[i].length; ++j) { 
-                
-                float x = j*gridSize;
-                float y = i*gridSize;
-                if (map[i][j] == CaveGenerationImpl.WALL) 
+        for (int i = 0; i < map.length; ++i) {
+            for (int j = 0; j < map[i].length; ++j) {
+
+                float x = j * gridSize;
+                float y = i * gridSize;
+                if (map[i][j] == CaveGenerationImpl.WALL)
                     shapeRenderer.setColor(Color.DARK_GRAY);
                 else {
                     shapeRenderer.setColor(Color.WHITE);
@@ -140,7 +143,7 @@ public class ProceduralGenerationScreen implements Screen {
         }
         shapeRenderer.end();
     }
-    
+
     @Override
     public void resize(int width, int height) {
         if (this.width == width && this.height == height) {
@@ -186,12 +189,12 @@ public class ProceduralGenerationScreen implements Screen {
         spriteBatch.dispose();
         shapeRenderer.dispose();
     }
-    
+
     class GameplayInput implements InputProcessor {
 
-        public GameplayInput() { 
+        public GameplayInput() {
         }
-        
+
         @Override
         public boolean keyDown(int keyCode) {
             Gdx.app.log(TAG, "key pressed: " + keyCode);
@@ -199,6 +202,18 @@ public class ProceduralGenerationScreen implements Screen {
             case Keys.G:
                 Gdx.app.log(TAG, "Regenerate cave!");
                 generateCave();
+                return true;
+            case Keys.I:
+                Gdx.app.log(TAG, "Initialize cave!");
+                caveGenerator.initialize();
+                return true;
+            case Keys.NUM_1:
+                Phase params1 = caveGenerator.getPhase(0);
+                caveGenerator.step(params1.min, params1.max);
+                return true;
+            case Keys.NUM_2:
+                Phase params2 = caveGenerator.getPhase(1);
+                caveGenerator.step(params2.min, params2.max);
                 return true;
             }
             return false;
@@ -215,7 +230,8 @@ public class ProceduralGenerationScreen implements Screen {
         }
 
         @Override
-        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        public boolean touchDown(int screenX, int screenY, int pointer,
+                int button) {
             if (button == Buttons.LEFT) {
                 // do something
             } else if (button == Buttons.RIGHT) {
@@ -242,7 +258,7 @@ public class ProceduralGenerationScreen implements Screen {
         @Override
         public boolean scrolled(int amount) {
             return false;
-        } 
-        
-    }    
+        }
+
+    }
 }
